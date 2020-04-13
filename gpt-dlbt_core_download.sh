f29c0ae54bb60685f8347fd2e228196dc2e11917
technopremium <48184440+technopremium@users.noreply.github.com>
2020-04-13 16:16:50 -0400

    Create Download sh for gpt-dlbt model
    Will download a zip file with scrappy and gpt-2 all models to a folder on $home

---
+++ gpt-dlbt_core_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## Will download both folders the scrapping web files and the GPT-2 folder with all models
+wget https://www.dropbox.com/s/rohwgse7x56wie6/ruben%20%20fernandez%20%20-%20autoblog_ai.zip?dl=0
+mv ruben%20%20fernandez%20%20-%20autoblog_ai.zip?dl=0 gpt-dlbt.zip
